45369de3775e5dd97baed627f3e6da46a5254d06
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-06-10 14:17:00 -0300

    ItemMenu versao inicial

--- src/Menu/MenuItem.js
+++ src/Menu/MenuItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './MenuItem.css'
 
 class MenuItem extends React.Component {
   constructor (props) {
@@ -6,15 +7,24 @@ class MenuItem extends React.Component {
     this.state = {
       name: this.props.name,
       value: this.props.value,
-      color: this.props.color
+      color: this.props.color,
+      icon: this.props.icon
     }
   }
 
   render () {
     return (
-      <a href='#' className='item'>
-        {this.state.name}
-      </a>
+      <div className='item' style={{ backgroundColor: this.state.color }}>
+        <div className='icon'> <i className={this.props.icon} /> </div>
+        <div className='text'>
+          <div className='title'>
+            {this.state.name}
+          </div>
+          <div className='count'>
+            {this.state.value}
+          </div>
+        </div>
+      </div>
     )
   }
 }
