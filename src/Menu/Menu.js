ed819cb067b69d0694d9b244212a9616ce2fcb60
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-06-10 14:17:26 -0300

    Estrutura basica do header/menu

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Menu.css'
 
+import MenuItem from './MenuItem'
+
 class Menu extends React.Component {
   constructor (props) {
     super(props)
@@ -13,10 +15,10 @@ class Menu extends React.Component {
     return (
       <div className='menu'>
         <ul>
-          <div>Visitors {this.state.visitors} </div>
-          <div>Users </div>
-          <div>Sales </div>
-          <div>Orders </div>
+          <MenuItem color='#29cb97' name='Visitors' value='500' icon='fas fa-eye fa-3x' />
+          <MenuItem color='#fec400' name='Users' value='100' icon='fas fa-users fa-3x' />
+          <MenuItem color='#4c84ff' name='Sales' value='200 ' icon='fas fa-dollar-sign fa-3x' />
+          <MenuItem color='#ca66ff' name='Orders' value='300 ' icon='fas fa-shopping-cart fa-3x' />
         </ul>
       </div>
     )
