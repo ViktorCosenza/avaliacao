440e6aa8eb55e7f2afa218f554042bad38e74777
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-06-10 19:19:55 -0300

    Componente Comentario

---
+++ src/reports/Comment.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Comment.css'
+
+class Comment extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      user: this.props.user,
+      image: this.props.image || 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png',
+      message: this.props.message || 'Voluptate consectetur commodo reprehenderit deserunt aliqua eiusmod fugiat nulla aute mollit anim id.',
+      time: this.props.time || '3 mins ago'
+    }
+  }
+
+  render () {
+    return (
+      <div className='comment'>
+        <div
+          className='avatar'
+          style={{ backgroundImage: `url(${this.state.image})` }}
+        />
+        <div className='message'>
+          {this.state.message}
+          <div className='time'>
+            {this.state.time}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default Comment
