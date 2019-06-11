169f911cdba98db8705a7d48eb89df1a567f792e
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-06-10 21:00:59 -0300

    versao funcional app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+import Menu from './Menu/Menu'
+import Graph from './Graph/Graph'
+import Reports from './reports/Reports'
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header>
+        <Menu> I am a children </Menu>
       </header>
+      <div className='main-content'>
+
+        <div className='graph'>
+          <header>
+            Site Traffic Overview
+          </header>
+          <Graph />
+        </div>
+
+        <div className='reports'>
+          <header> Reports </header>
+          <Reports />
+        </div>
+
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
