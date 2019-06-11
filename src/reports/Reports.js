2519585f247e72068d43dd6f5edfd20fc0eb34d7
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-06-10 22:20:59 -0300

    get request comments

--- src/reports/Reports.js
+++ src/reports/Reports.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import './Reports.css'
 
 import Comment from './Comment'
@@ -7,22 +8,31 @@ class Reports extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      comments: this.props.comments
+      comments: 'Cant get comments'
     }
+  }
+
+  componentDidMount () {
+    axios.get('https://my-json-server.typicode.com/viktorcosenza/avaliacao/reports')
+      .then(response => {
+        console.log(response)
+        const comments = response.data.map(comment =>
+          <Comment
+            user={comment.user}
+            image={comment.image}
+            message={comment.message}
+            time={comment.time}
+          />
+        )
+        this.setState({ comments: comments })
+      })
   }
 
   render () {
     return (
       <div className='comments-container'>
         <div className='comment-list'>
-          <Comment />
-          <Comment />
-          <Comment />
-          <Comment />
-          <Comment />
-          <Comment />
-          <Comment />
-          <Comment />
+          {this.state.comments}
         </div>
         <form className='new-comment'>
           <input type='text' placeholder='Type your comment here...' />
