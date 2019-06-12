44016985ec458ea8254546d94c141878d42f9600
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2019-06-11 21:19:06 -0300

    Request e ajustes finos css

--- src/Graph/Graph.js
+++ src/Graph/Graph.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import './Graph.css'
 
 import { Bar } from 'react-chartjs-2'
@@ -7,10 +8,40 @@ class Graph extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      data: props.data,
+      data: {},
       options: props.options,
       legend: props.legend
     }
+  }
+
+  static defaultProps = {
+    displayTitle:false,
+    displayLegend: false,
+  }
+
+  componentDidMount () {
+    axios.get('https://my-json-server.typicode.com/viktorcosenza/avaliacao/sales')
+      .then(response => {
+        const months = []
+        const values = []
+        for (const datapoint of response.data) {
+          months.push(datapoint.month)
+          values.push(datapoint.sales)
+        }
+        this.setState({
+          data: {
+            labels: months,
+            datasets: [
+              {
+                label: '',
+                data: values,
+                backgroundColor: '#4c84ff'
+              }
+            ]
+          }
+        })
+      })
+      .catch(err => console.log(err))
   }
 
   render () {
@@ -18,8 +49,17 @@ class Graph extends React.Component {
       <div className='bar-chart'>
         <Bar
           data={this.state.data}
-          options={this.state.options}
-          legend={this.state.legend}
+          options={{
+            title:{
+              display: this.props.displayTitle,
+              text: this.props.location,
+              fontSize:25
+            },
+            legend:{
+              display: this.props.displayLegend,
+              position: this.props.legendPosition
+            }
+          }}
         />
       </div>
     )
